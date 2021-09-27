70778319f4f0755fadaa5e7db2f3a123c32110d8
d-rocha <dr.oliveira88@gmail.com>
2021-09-26 21:54:50 -0300

    :sparkles: Inital commit
    Initial commit

---
+++ app/src/main/java/com/example/kotlin_assessment/ReadItemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_assessment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReadItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read_item)
+    }
+}
